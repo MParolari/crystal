3349f664fa095a60a07fedac4c59388a820b1cce
MParolari <mparolari.dev@gmail.com>
2019-05-02 17:19:34 +0200

    VHT skew error in Llog

--- net/crystal/crystal.c
+++ net/crystal/crystal.c
@@ -193,6 +193,7 @@ static time_h_t t_ref_corrected_h;   // reference time acquired during the S or 
 
 // for clock logging and measurement
 static time_h_t log_t_ref_h_s;  // for clock skew measurement
+static skew_t log_skew_error;   // for skew log
 
 // since RTimer is 32kHz and DCO 4MHz: (4MHz / 32kHz == 128 == CLOCK_PHI), so
 // 7bit are enough for the high reference offset; in practice we have:
@@ -732,6 +733,11 @@ PT_THREAD(s_node_thread(struct rtimer *t, void* ptr))
     if (tmp_h > t_ref_corrected_h) {
       t_ref_corrected_h_s = tmp_h;
       t_ref_corrected_h = tmp_h;
+      // error between the corrected ref time and the estimated one
+      if (t_ref_corrected_h_s >= t_ref_estimated_h)
+        log_skew_error = (skew_t)(t_ref_corrected_h_s - t_ref_estimated_h);
+      else
+        log_skew_error = -((skew_t)(t_ref_estimated_h - t_ref_corrected_h_s));
     }
     else { // use the estimate if didn't update
       t_ref_corrected_h = t_ref_estimated_h;
@@ -1026,6 +1032,7 @@ static char node_main_thread(struct rtimer *t, void *ptr) {
     init_epoch_state();
     crystal_info.n_ta = 0;
     log_t_ref_h_s = 0;
+    log_skew_error = 0;
 
     if (!skip_S) {
       RADIO_OSC_ON();
@@ -1180,7 +1187,7 @@ void crystal_print_epoch_logs() {
     printf("S %u:%u %u %u:%u %d %u\n", epoch, n_ta_tx, n_all_acks, synced_with_ack, sync_missed, period_skew, hopcount);
     printf("P %u:%u %u %u:%u %u %u %d:%d\n", 
         epoch, recvsrc_S, recvtype_S, recvlen_S, n_badtype_A, n_badlen_A, n_badcrc_A, log_ack_skew_err, 0);
-    printf("L %u %llu\n", epoch, log_t_ref_h_s);
+    printf("L %u %llu %ld\n", epoch, log_t_ref_h_s, log_skew_error);
   }
 
 #if CRYSTAL_LOGLEVEL == CRYSTAL_LOGS_ALL
